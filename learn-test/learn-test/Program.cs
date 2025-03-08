7e4556547cdb798b46dae3f8e4b0a9afcc73f839
yourunb <bxyura@gmail.com>
2025-03-08 10:27:05 +0300

    refactor: update example

--- learn-test/learn-test/Program.cs
+++ learn-test/learn-test/Program.cs
@@ -2,8 +2,10 @@
 string b = "a";
 b += "bc";
 Console.WriteLine("Строковые переменные a == b: " + (a == b));
+Console.WriteLine("Объекты со строками a == b: " + ((object)a == (object)b));
 
 decimal myD = 1.5m;
 double x = (double)myD;
 myD = (decimal)x;
 Console.WriteLine("Это тип {0}", (9 * Math.PI).GetType()); //вернет тип System.Double
+
