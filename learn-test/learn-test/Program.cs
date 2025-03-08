ed84fe337f210e77462322a5e4d26af941a28518
yourunb <bxyura@gmail.com>
2025-03-08 10:25:27 +0300

    feat: add new example

--- learn-test/learn-test/Program.cs
+++ learn-test/learn-test/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string a = "abc";
+string b = "a";
+b += "bc";
+Console.WriteLine("Строковые переменные a == b: " + (a == b));
+
+decimal myD = 1.5m;
+double x = (double)myD;
+myD = (decimal)x;
+Console.WriteLine("Это тип {0}", (9 * Math.PI).GetType()); //вернет тип System.Double
